e01c6068aa7ae0ff6c2301630b8644757cb84480
AgustinArielGalarza <agustin.galarza96@gmail.com>
2020-02-13 15:23:24 -0300

    Primeros pasos JPA, Claso Pojo Cliente con las configuracion para que resiva datos de la bbdd

---
+++ src/main/java/com/bolsadeideas/springboot/jpa/app/model/entity/Cliente.java
@@ -0,0 +1,79 @@
+package com.bolsadeideas.springboot.jpa.app.model.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity // para indicarle que una Clase Pojo esta relacionada a JPA se le especifica con
+		// esta anotacion
+@Table(name = "clientes") // para que no se llame igual que la tabla de la bbdd. con esto configuramos.
+public class Cliente implements Serializable { // se recomienda implementar serializable porque cuando se transmite
+												// datos a JPA o web se parsea a baits
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nombre;
+	private String apellido;
+	private String email;
+
+	@Column(name = "create_at")
+	@Temporal(TemporalType.DATE)
+	private Date createAt;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
